3b5123cdd9ea6013f28f07cf3da83e01cc4383af
joharbatta <joharbatta@gmail.com>
2019-09-10 23:53:31 +0530

    cutropes

---
+++ Test Questions/cutropes.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+public class cutropes
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+int z;
+
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]==arr[i+1])
+            {
+                i++;
+                System.out.print(n-i-1);
+            }
+            else
+            {
+                System.out.print(n-i-1);
+            }
+
+        }
+    }
+
+}
