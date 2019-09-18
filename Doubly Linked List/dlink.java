5423a32a4d2f4b9c6b944baac2d594c5769240a8
joharbatta <joharbatta@gmail.com>
2019-09-19 01:53:56 +0530

    practice doubly linked list

---
+++ Doubly Linked List/dlink.java
@@ -0,0 +1,81 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int data)
+    {
+        this.data=data;
+    }
+
+}
+class DoublyLinkedList
+{
+    private Node head;
+    private Node tail;
+    int size;
+    DoublyLinkedList()
+    {
+        head=null;
+        tail=null;
+        size=0;
+    }
+
+    public void insertLast(int data)
+    {
+        Node newNode=new Node(data);
+        if(isEmpty())
+        {
+            head=newNode;
+        }
+        else
+        {
+            tail.next=newNode;
+        }
+        newNode.prev=tail;
+        tail=newNode;
+        size++;
+
+    }
+
+    public void displayForward()
+    {
+        if(head==null)
+        {
+            return;
+        }
+        Node temp=head;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println("Null");
+
+    }
+
+
+    public boolean isEmpty()
+    {
+        return size==0; //head==null
+    }
+
+    public int length()
+    {
+        return size;
+    }
+}
+class dlink
+{
+    public static void main(String[] args) {
+
+        DoublyLinkedList d=new DoublyLinkedList();
+        d.insertLast(3);
+        d.insertLast(4);
+        d.insertLast(8);
+        d.displayForward();
+
+
+    }
+}
