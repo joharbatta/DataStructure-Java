2e4eaaffdea7369af9bc9bc6dd2fcd41f5973d4c
joharbatta <joharbatta@gmail.com>
2020-01-21 21:52:44 +0530

    reverse word method 2

--- String/ReverseWords.java
+++ String/ReverseWords.java
@@ -1,12 +1,34 @@
-import java.util.*;
-
+import java.util.Scanner;
+import java.util.Stack;
 public class ReverseWords{
    public static void main(String args[]) 
    {
        Scanner s = new Scanner(System.in);
        String str = s.nextLine();
        reverseWords(str);
-   }
+       reverseWords1(str);
+   }  
+   static void reverseWords1(String s)
+   {
+        String[] words=s.split("\\s");
+        StringBuilder d=new StringBuilder();
+    //    String result="";
+        for(int i=0;i<words.length;i++)
+        {
+            String reverse="";
+            String k=words[i];
+            int n=k.length()-1;
+           for(int p=n;p>=0;p--)
+            {
+                reverse = reverse + k.charAt(p);
+            }
+           //words[i]=reverse;
+           d.append(reverse);
+           d.append(" ");
+        }
+        System.out.println(d);
+    }
+
 
    static void reverseWords(String s) 
    {
