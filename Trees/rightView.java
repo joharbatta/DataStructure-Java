51b2d675c4bb58b03f5c2cb7993da4de0492255f
joharbatta <joharbatta@gmail.com>
2020-03-27 01:02:08 +0530

    BFS Iterative  method right view

--- Trees/rightView.java
+++ Trees/rightView.java
@@ -38,7 +38,40 @@ class BinaryTree{
         rightView(root.left,level+1);
        
     }
-   
+    //iterative
+    void rightView1(Node root)
+    {
+        if(root==null) 
+        return;
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        while(true)
+        {
+            int size=q.size();
+            if(size==0)
+            {
+            break;
+            }
+            for(int i=0;i<size;i++)
+            {
+                Node temp=q.remove();
+                if(i==size-1)
+                {
+                    System.out.print(temp.data+" ");
+                }
+                if(temp.left!=null)
+                {
+                    q.add(temp.left);
+                }
+                if(temp.right!=null)
+                {
+                    q.add(temp.right);
+                }
+            }
+
+        }
+    }
+
 }
 
 class rightView{
@@ -49,6 +82,8 @@ class rightView{
         bt.root.right=new Node(5);
         bt.root.left.right=new Node(9);
         bt.root.right.left=new Node(7);
-        bt.rightView(bt.root,1);  
+        bt.rightView(bt.root,1);
+        bt.rightView1(bt.root);
+        
     }
 }
