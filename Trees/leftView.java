6f85af179234aabc19368ba7004f1e730a88b338
joharbatta <joharbatta@gmail.com>
2020-03-26 20:14:46 +0530

    LEFT VIEW TREE Iterative

--- Trees/leftView.java
+++ Trees/leftView.java
@@ -1,3 +1,8 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.management.Query;
+
 class Node
 {
     int data;
@@ -33,10 +38,44 @@ class BinaryTree{
         leftView(root.left,level+1);
         leftView(root.right,level+1);
     }
+    //iterative
+    void leftView1(Node root)
+    {
+        if(root==null) 
+        return;
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        while(true)
+        {
+            int size=q.size();
+            if(size==0)
+            {
+            break;
+            }
+            for(int i=0;i<size;i++)
+            {
+                Node temp=q.remove();
+                if(i==0)
+                {
+                System.out.print(temp.data+" ");
+                }
+                if(temp.left!=null)
+                {
+                    q.add(temp.left);
+                }
+                if(temp.right!=null)
+                {
+                    q.add(temp.right);
+                }
+            }
+
+        }
+    }
+
 
 }
 
-class btree{
+class leftView{
     public static void main(String[] args) {
 
         BinaryTree bt=new BinaryTree(2);
@@ -44,6 +83,8 @@ class btree{
         bt.root.right=new Node(5);
         bt.root.left.right=new Node(9);
         bt.root.right.left=new Node(7);
+        bt.leftView(bt.root,1);
+        bt.leftView1(bt.root);
         
     }
 }
